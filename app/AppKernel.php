77a2c3d6d45fff8f159aea88dcca097a3c3f0a21
nouran <nouran.nayel@hotmail.com>
2018-05-28 04:34:46 +0200

    Install JMS/Serializer Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Todo\Bundle\UserBundle\UserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
